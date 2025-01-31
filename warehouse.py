b7719dae32ad085aaf755093025c4eea55256e0a
Jacobo149 <jtp323@lehigh.edu>
2025-01-31 13:44:32 -0500

    Created Warehouse Class

---
+++ warehouse.py
@@ -0,0 +1,15 @@
+# Class to represent a warehouse.
+class warehouse:
+    def __init__(self, ID):
+        self.ID = ID
+        self.stock = {}
+
+    def add_transaction(self, transaction):
+        if transaction.item not in self.stock:
+            self.stock[transaction.item] = [transaction.number, transaction.price]
+        self.stock[transaction.item] += transaction.number
+
+
+    def __str__(self):
+        for item in self.stock:
+            return f'{self.ID} {item} {self.stock[item][0]} {self.stock[item][1]}'
