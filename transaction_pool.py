fd4aae52ce673ecd1e7f3a75282a1c73287ba9ee
Jacobo149 <jtp323@lehigh.edu>
2025-01-31 13:30:43 -0500

    Transaction Pool Created

---
+++ transaction_pool.py
@@ -0,0 +1,16 @@
+# Class to hold non-processed transactions.
+class transaction_pool:
+    def __init__(self):
+        self.pool = []
+
+    def add_transaction(self, transaction):
+        self.pool.append(transaction)
+
+    def get_transactions(self):
+        return self.pool
+    
+    def remove_transaction(self, transaction):
+        self.pool.remove(transaction)
+    
+    def __str__(self):
+        return '\n'.join([str(transaction) for transaction in self.pool])
